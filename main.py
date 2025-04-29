dca89c969f7fb307d1f47c787326128048ed10bf
paullebric <paullebric@gmail.com>
2025-04-29 15:55:55 +0200

    Glucose diffusion par gradient qui marche

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-import pygame as pg
 
 
 
--- repartition_sucre.py
+++ repartition_sucre.py
@@ -2,28 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
-import pygame as pg
 
-
-m_taille = 20 # Taille de la matrice
-add_glucose = 50 # nb d'itérations avant l'ajout de glucose
+m_taille = 50 # Taille de la matrice
+add_glucose = 10 # nb d'itérations avant l'ajout de glucose
 
 def init_matrice(taille):
     """Initialise une matrice de taille donnée avec des zeroes."""
     return np.zeros((taille, taille))
 
+#permet de mettre du sucre a un endroit donné de la matrice pour un certain amount et une certaine area
 def sucre_input(mat,position,amount,area):
     x=position[0]
     y=position[1]
     for i in range(x-area,x+area):
         for j in range(y-area,y+area):
-            print("x",x,"y",y)
             if mat[i][j]+amount>1:
                 mat[i][j]=1
             else:
                 mat[i][j]+=amount
     return mat
 
+#permet d'afficher la matrice avec une échelle de couleur grace à mathplotlib
 def afficher_matrice(mat, titre=""):
     plt.imshow(mat, cmap='YlOrRd', vmin=0, vmax=1)
     plt.colorbar(label='Concentration de sucre')
@@ -31,6 +30,7 @@ def afficher_matrice(mat, titre=""):
     plt.axis('off')  # cache les axes
     plt.show()
 
+#update la matrice de sucre en le diffusant dans les cases voisines en fonction du gradient de concentration
 def update_sucre(old_mat):
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
     diffusion_mat=np.zeros((m_taille, m_taille))
@@ -46,14 +46,12 @@ def update_sucre(old_mat):
                         diffusion_mat[ni][nj] += diffusion
                         diffusion_mat[i][j] -= diffusion
     new_mat=old_mat +diffusion_mat
-    return new_mat
-            
-            
-mat = init_matrice(m_taille)
+    return new_mat  
 
+mat = init_matrice(m_taille)
 #animation
 fig, ax = plt.subplots()
-im = ax.imshow(mat, cmap='YlOrRd', vmin=0, vmax=1)
+im = ax.imshow(mat, cmap='turbo', vmin=0, vmax=0.2)
 plt.colorbar(im, ax=ax)
 plt.title("Diffusion de sucre")
 title = ax.set_title("Diffusion de sucre")
@@ -63,9 +61,9 @@ def update(frame):
     mat = update_sucre(mat)
     if frame%add_glucose==0:
         mat = sucre_input(mat,(m_taille//2, m_taille//2), 0.5,1)
+    print("Frame:", frame)
     im.set_array(mat)
     return [im]
-
 # Lancement de l'animation
 ani = animation.FuncAnimation(fig, update, frames=100000, interval=1, blit=True)
 plt.show()
