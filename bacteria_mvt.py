7909e2cc21eb850771a5b9ca97e8f4d7147c8bcb
paullebric <paullebric@gmail.com>
2025-06-05 10:31:56 +0200

    tout est plus beau

--- bacteria_mvt.py
+++ bacteria_mvt.py
@@ -1,99 +1,111 @@
 """
-Ce fichier décrit le mouvement des bactéries dans le bio-réacteur
-Les bactéries sont approximés selon des points ayant des coordonées x,y à tout moment t
+Ce fichier décrit le mouvement des bactéries dans le bio-réacteur.
+Les bactéries sont approximées par des points ayant des coordonnées x, y à tout moment t.
 """
+
 import numpy as np
 import random as rd
-#from main import *
-vision_b = 5 #vision des bactéries
-lambda1 = 1 #Vitesse de déplacement des bactéries selon le gradient
-lambda2 = 0 #Amplitude de l'aléatoire dans le déplacement des bactéries
-hypothese1 = True#les bacteries se déplacent selon le gradient de concentration de glucose
-hypothese2 = not hypothese1 #quantité de glucose consommée par les bactéries à chaque itération
-cout_atp_mouvement = 10 #coût d'ATP pour se déplacer
-kgainatp = 100 #gain d'ATP pour manger du glucose
-cout_mitose = 300 #coût d'ATP pour se diviser
-threshold_mitose = 200 #seuil d'ATP pour se diviser
-kglucose = 0.05 #quantité de glucose consommée par les bactéries à chaque itération
 
+# ===================== PARAMÈTRES GLOBAUX =====================
+VISION = 5                     # Distance de vision des bactéries
+LAMBDA_GRADIENT = 0.01         # Force du déplacement vers le gradient
+LAMBDA_RANDOM = 0              # Intensité du mouvement brownien (aléatoire)
+USE_GRADIENT = True            # Hypothèse 1 : déplacement selon gradient
+USE_RANDOM = not USE_GRADIENT  # Hypothèse alternative
+
+ATP_COST_MOVE = 10             # Coût d'ATP pour un déplacement
+ATP_GAIN_EAT = 100             # Gain d'ATP en consommant du glucose
+ATP_COST_MITOSIS = 300         # Coût d'ATP pour une division
+ATP_THRESHOLD_MITOSIS = 200    # Seuil d'ATP pour se diviser
+GLUCOSE_CONSUMPTION = 0.05     # Quantité de glucose consommée par itération
+
+# ===================== CLASSE BACTERIA =====================
 class Bacteria:
-    def __init__(self, b_posx,b_posy):
+    def __init__(self, b_posx, b_posy):
         self.nom = 'E.coli'
         self.posx = b_posx
         self.posy = b_posy
-        self.posmaty = 0
         self.posmatx = 0
+        self.posmaty = 0
         self.ATP = 100
-        self.pop_threshold = 10 #seuil de surpopulation pour la mort dans une case
-        self.death_pop_chance = 0.8 #taut de chance de mourir si la case est surpeuplée
+        self.pop_threshold = 10        # Seuil de surpopulation
+        self.death_pop_chance = 0.8    # Proba de mourir en cas de surpopulation
         self.death = False
         self.Etotal = 0
         self.gradx = 0
         self.grady = 0
         self.gradnumx = 0
         self.gradnumy = 0
-        
-    def Gradient(self,matrice):
-        gluc_tot = np.sum(matrice)
-        gradx=0
-        grady=0  
-        for i in range(self.posmaty-vision_b, self.posmaty+vision_b):
-            for j in range(self.posmatx-vision_b, self.posmatx+vision_b):
-                if 0<i<matrice.shape[0] and 0<j<matrice.shape[1]:
-                    hypothenuse=((self.posmatx - j)**2 + (self.posmaty - i)**2)**0.5
-                    if hypothenuse != 0:  # éviter la division par zéro
-                        delta_distance_x= (j - self.posmatx) / hypothenuse**3
-                        delta_distance_y= (i - self.posmaty) / hypothenuse**3
-                        gradx += matrice[i][j]/gluc_tot * delta_distance_x
-                        grady += matrice[i][j]/gluc_tot * delta_distance_y
-        gradnx = gradx/(gradx**2+grady**2)**0.5
-        gradny = grady/(gradx**2+grady**2)**0.5
-        self.gradx = gradnx
-        self.grady = gradny
 
+    # ===== Gradient perçu par la bactérie =====
+    def Gradient(self, matrice):
+        gluc_tot = np.sum(matrice)
+        gradx = grady = 0
+
+        for i in range(self.posmaty - VISION, self.posmaty + VISION):
+            for j in range(self.posmatx - VISION, self.posmatx + VISION):
+                if 0 < i < matrice.shape[0] and 0 < j < matrice.shape[1]:
+                    dx = j - self.posmatx
+                    dy = i - self.posmaty
+                    dist = (dx**2 + dy**2)**0.5
+                    if dist != 0:
+                        coeff = matrice[i][j] / gluc_tot / dist**3
+                        gradx += coeff * dx
+                        grady += coeff * dy
+
+        norm = (gradx**2 + grady**2)**0.5 or 1
+        self.gradx = gradx / norm
+        self.grady = grady / norm
+
+    # ===== Mise à jour de la position =====
     def update_b_pos(self, matrice):
-        m_taille = matrice.shape[0]
-        self.posmatx = int(m_taille * self.posx)
-        self.posmaty = int(m_taille * self.posy)
+        taille = matrice.shape[0]
+        self.posmatx = int(self.posx * taille)
+        self.posmaty = int(self.posy * taille)
+
         self.Gradient(matrice)
+
         brownx = rd.uniform(-1, 1)
         browny = rd.uniform(-1, 1)
-        # ajout d’un biais vers le gradient
-        newposx = self.posx + lambda2 * brownx + lambda1 * self.gradx
-        newposy = self.posy + lambda2 * browny + lambda1 * self.grady
-        #debug des murs : si la posx ou posy sort de la matrice on n'avance pas
-        if 0 <= newposx <= 1 : self.posx=newposx
-        if 0 <= newposy <= 1 : self.posy=newposy
-        #update de la position de la bactérie dans la matrice
-        self.ATP -= cout_atp_mouvement
-        
-    def update_death_and_mitosis(self,matrice,list_b):
-        #mort de la bactérie si il y a trop de bactéries sur la case
-        nb_bacteries_case = 0
-        if self.ATP <=0:
-            self.death = True
-        for bact in list_b:
-            if bact.posmatx == self.posmatx and bact.posmaty == self.posmaty:
-                nb_bacteries_case += 1
-        if nb_bacteries_case >= self.pop_threshold:
-            if rd.choices([True, False], [self.death_pop_chance, 1-self.death_pop_chance]):
-                self.death = True
-        #mitosis si il y a assez d'ATP et pas trop de bactéries sur la case
-        if nb_bacteries_case < self.pop_threshold and self.ATP > threshold_mitose:    
-                # On crée une nouvelle bactérie à une position légèrement différente
-                dif_de_pos = 0.001
-                newposx = self.posx + rd.uniform(-dif_de_pos, dif_de_pos)
-                newposy = self.posy + rd.uniform(-dif_de_pos, dif_de_pos)
-                if 0<newposx<1 and 0<newposy<1:
-                    new_bacteria = Bacteria(newposx,newposy)
-                    self.ATP -= cout_mitose
-                    return new_bacteria
-            
-    def update_eat(self, matrice):
-        # On mange le glucose de la case de la matrice
-        if matrice[self.posmaty][self.posmatx] > kglucose*3:   # On mange que si il y a assez de glucose    
-            matrice[self.posmaty][self.posmatx] -= kglucose # Consommation de glucose 
-            self.ATP += kgainatp # On gagne de l'ATP
-        return matrice 
 
-        
+        newposx = self.posx + LAMBDA_RANDOM * brownx + LAMBDA_GRADIENT * self.gradx
+        newposy = self.posy + LAMBDA_RANDOM * browny + LAMBDA_GRADIENT * self.grady
+
+        if 0 <= newposx <= 1:
+            self.posx = newposx
+        if 0 <= newposy <= 1:
+            self.posy = newposy
+
+        self.ATP -= ATP_COST_MOVE
+
+    # ===== Mort et mitose =====
+    def update_death_and_mitosis(self, matrice, list_b):
+        if self.ATP <= 0:
+            self.death = True
+            return
+
+        # Compte les bactéries dans la même case
+        nb_same_cell = sum(
+            1 for bact in list_b if bact.posmatx == self.posmatx and bact.posmaty == self.posmaty
+        )
+
+        if nb_same_cell >= self.pop_threshold:
+            if rd.choices([True, False], [self.death_pop_chance, 1 - self.death_pop_chance])[0]:
+                self.death = True
+                return
+
+        # Mitose
+        if nb_same_cell < self.pop_threshold and self.ATP > ATP_THRESHOLD_MITOSIS:
+            dif = 0.001
+            newx = self.posx + rd.uniform(-dif, dif)
+            newy = self.posy + rd.uniform(-dif, dif)
+            if 0 < newx < 1 and 0 < newy < 1:
+                self.ATP -= ATP_COST_MITOSIS
+                return Bacteria(newx, newy)
+
+    # ===== Consommation de glucose =====
+    def update_eat(self, matrice):
+        if matrice[self.posmaty][self.posmatx] > 3 * GLUCOSE_CONSUMPTION:
+            matrice[self.posmaty][self.posmatx] -= GLUCOSE_CONSUMPTION
+            self.ATP += ATP_GAIN_EAT
+        return matrice
--- main.py
+++ main.py
@@ -1,66 +1,82 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import random as rd
 from repartition_sucre import *
 from bacteria_mvt import Bacteria
 from Energie_bact import *
-m_taille = 3 # Taille de la matrice
-add_glucose = 10 # intervalle d'ajout de glucose
 
-# Initialisation de la matrice et des bactéries
-mat = init_matrice(m_taille)
-list_b = [Bacteria(rd.uniform(0, 1), rd.uniform(0, 1)) for _ in range(3)]
+# ===================== PARAMÈTRES =====================
+M_TAILLE = 50                     # Taille de la matrice
+NB_BACTERIES_INIT = 100         # Nombre de bactéries au début
+ADD_GLUCOSE_INTERVAL = 10       # (non utilisé ici, mais prévu)
+DUREE_GLUCOSE = 200000         # Jusqu'à quand on ajoute du sucre
+DUREE_DIFFUSION = 10000        # Jusqu'à quand on diffuse le sucre
+DEBUT_BACTERIES = 1            # Quand les bactéries commencent à agir
+MAX_ITER = 1000                # Pour un test rapide (change selon besoin)
+SUCRE_POS = (M_TAILLE//2, M_TAILLE//2)              # Position d'injection de sucre
+SUCRE_CONCENTRATION = 0.3       # Concentration injectée
+SUCRE_RAYON = 1                 # Rayon de diffusion du sucre
+BACTERIE_COLOR = 'white'
+BACTERIE_SIZE = 10
 
-# Animation setup
+# ===================== INITIALISATION =====================
+mat = init_matrice(M_TAILLE)
+list_b = [Bacteria(rd.uniform(0, 1), rd.uniform(0, 1)) for _ in range(NB_BACTERIES_INIT)]
+
+# Setup animation
 fig, ax = plt.subplots()
-im = ax.imshow(mat, cmap='turbo', vmin=0, vmax=1, extent=[0, m_taille, 0, m_taille], origin='lower')
+im = ax.imshow(mat, cmap='turbo', vmin=0, vmax=1, extent=[0, M_TAILLE, 0, M_TAILLE], origin='lower')
 plt.colorbar(im, ax=ax)
 title = ax.set_title("Bioreacteur avec sucre et bactéries")
+x_bact = [b.posx * M_TAILLE for b in list_b]
+y_bact = [b.posy * M_TAILLE for b in list_b]
+sc = ax.scatter(x_bact, y_bact, c=BACTERIE_COLOR, s=BACTERIE_SIZE)
 
-# Initialisation du scatter plot pour les bactéries
-x_bact = [b.posx * m_taille for b in list_b]
-y_bact = [b.posy * m_taille for b in list_b]
-sc = ax.scatter(x_bact, y_bact, c='white', s=10)  # points blancs pour les bactéries
-
-# Fonction d'animation = boucle principale
+# ===================== FONCTION D'ANIMATION =====================
 def update(frame):
-    global mat
-    steps_per_frame = 1  # vitesse de l'animation
+    global mat, list_b
+    steps_per_frame = 1
     for _ in range(steps_per_frame):
-        #gradglobal(list_b, mat)  # Mise à jour du gradient global pour chaque bactérie
         iteration = frame * steps_per_frame + _
-        if iteration < 200000: # Ajout de glucose au début de la simulation jusqqquaa n simulaation
-                mat = sucre_input(mat, (0, 0), 0.3, 1)
-                # mat = sucre_input(mat, (12, 10), 0.5, 1)
-                # mat = sucre_input(mat, (20, 22), 1, 1)
-        if iteration < 10000: # Loi de flick = repartition du glucose jusqu'à n frames
-                mat = update_sucre(mat)
-        #les bacteries existe a partir de ce moment
-        if True:
-            if iteration > 20:
-                for bacterie in list_b:
-                    if bacterie.death == True:
-                        list_b.remove(bacterie)
-                    # Mise à jour de la position des bactéries
-                    bacterie.update_b_pos(mat)
-                    if True :
-                        new_bact = bacterie.update_death_and_mitosis(mat,list_b)
-                        if isinstance(new_bact,Bacteria):
-                            list_b.append(new_bact)
-                        mat = bacterie.update_eat(mat)
-    print(len(list_b), "bactéries vivantes")
-    for bact in list_b:
-        print(bact.gradnumx, bact.gradnumy)
-    if iteration == 1000:print("fin"),exit()
-    im.set_array(mat)
-    # Mise à jour des positions des bactéries
-    x_bact = [b.posx * m_taille for b in list_b]
-    y_bact = [b.posy * m_taille for b in list_b]
+
+        # --- Ajout de glucose ---
+        if iteration < DUREE_GLUCOSE:
+            mat = sucre_input(mat, SUCRE_POS, SUCRE_CONCENTRATION, SUCRE_RAYON)
+
+        # --- Diffusion (loi de Fick) ---
+        if iteration < DUREE_DIFFUSION:
+            mat = update_sucre(mat)
+
+        # --- Bactéries (après un certain temps) ---
+        if iteration > DEBUT_BACTERIES:
+            new_bacts = []
+            for bacterie in list_b[:]:  # Copie pour éviter modification pendant itération
+                if bacterie.death:
+                    list_b.remove(bacterie)
+                    continue
+                bacterie.update_b_pos(mat)
+                new_bact = bacterie.update_death_and_mitosis(mat, list_b)
+                if isinstance(new_bact, Bacteria):
+                    new_bacts.append(new_bact)
+                mat = bacterie.update_eat(mat)
+            list_b.extend(new_bacts)
+
+    # --- Affichage / mise à jour graphique ---
+    x_bact = [b.posx * M_TAILLE for b in list_b]
+    y_bact = [b.posy * M_TAILLE for b in list_b]
     sc.set_offsets(np.c_[x_bact, y_bact])
+    im.set_array(mat)
+
+    # --- Affichage console ---
+    print(len(list_b), "bactéries vivantes")
+
+    if iteration == MAX_ITER:
+        print("Fin de la simulation.")
+        exit()
+
     return [im, sc]
 
-ani = animation.FuncAnimation(fig, update, frames=10000, interval=100, blit=True)
+# ===================== LANCEMENT =====================
+ani = animation.FuncAnimation(fig, update, frames=10_000, interval=100, blit=True)
 plt.show()
-
-
-
